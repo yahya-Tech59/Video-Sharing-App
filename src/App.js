9d022e06a4bcb2b2985103bd0c282ec4bc0b8ab5
yahya-cade <yahyekade790@gmail.com>
2023-09-11 13:03:47 +0300

    added components search and videoDetail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import "./App.css";
 import { Grid } from "@mui/material";
 import youtube from "./api/youtube";
+import { SearchBar } from "./components/SearchBar";
+import { VideoList } from "./components/VideoList";
+import { VideoDetail } from "./components/VideoDetail";
+import { useState } from "react";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideos] = useState({ id: {}, snippet: {} });
+
+  const handleSubmit = async (searchItem) => {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyCYfOBo6d2nolRI9XcKSa-WhCZS-Njcs9c",
+        q: searchItem,
+      },
+    });
+    setVideos(videos);
+    setSelectedVideos(videos[0]);
+  };
+
   return (
-    <div className="App">
-      <h1>Video Sharing Application</h1>
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail videos={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
